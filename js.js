45bcce0c8a0e22952207d90407a4999f3767b75b
KiraBi1234 <kirabi493gmail.com>
2023-01-04 17:48:04 +0200

    First commit

---
+++ js.js
@@ -0,0 +1,42 @@
+function block1(){
+    let elem = document.getElementById("Block1");
+    elem.style.fontFamily = "serif"
+    elem.style.color = "red"
+}
+
+function block2() {
+    let elem = document.addEventListener("Block2");
+ }
+
+
+
+function block3() {
+    document.body.style.background = `rgb(${rnd(255)}, ${rnd(255)}, ${rnd(255)})`;
+}
+  
+function rnd(arg) {
+    return Math.floor(Math.random() * arg);
+}
+
+function blink(){
+    text.hidden = !text.hidden; 
+
+}
+
+
+function block4() {
+    let elem = document.getElementById("Block4");
+    
+    elem.style.color = "red"
+
+}
+
+
+function block5(){
+    let elem = document.getElementById("Block5");
+    elem.style.fontFamily = "serif"
+    elem.style.color = "grin"
+}
+
+
+    
